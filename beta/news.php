ac529ef2bbb0881e7a3fd5185c7a8423a464ac88
jojomoore2007 <31520139+jojomoore2007@users.noreply.github.com>
2018-09-10 13:53:25 -0400

    Delete news.php

--- beta/news.php
+++
@@ -1,7 +0,0 @@
-<html>
-$vlc = "C:\Program Files (x86)\VideoLan\VLC\vlc.exe"; // or whatever the path to vlc is;
-$path_file = "rtp://239.219.90.22:50000"; // you can also retrieve that with a relative path like using realpath(relative_path);
-
-// launch vlc
-pclose(popen("start \"$vlc\" \"$path_file\"", "r"));
-</html>
